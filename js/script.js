ee1bab6d93e9c05073f07d89bf57706f3ed879e8
timoyxgit <mayonsirelletimothy@gmaill.com>
2026-02-19 16:00:36 +0800

    WSAT-M4: JavaScript interactivity added

--- js/script.js
+++ js/script.js
@@ -15,7 +15,18 @@ submitBtn.addEventListener("click",function(event){
 const themeToggle = document.getElementById("themeToggle");
     themeToggle.addEventListener("click",function(){
     document.body.classList.toggle("dark-mode");
+
 });
+
+themeToggle.addEventListener("click",function() {
+    if (this.textContent == "Toggle Dark Mode") {
+        this.textContent = "Toggle Light Mode";
+     } 
+    else {
+        this.textContent = "Toggle Dark Mode";
+    } 
+});
+
 
 const toggleSkills = document.getElementById("toggleSkills");
 const skillsSection = document.getElementById("skillsSection");
